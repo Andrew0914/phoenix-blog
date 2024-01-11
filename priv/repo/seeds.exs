436da1a4aa3eb98816d22c3fecde06ca113d4649
Andrew González <andrewalangm@gmail.com>
2024-01-11 17:01:54 -0600

    Creating posts resources ecto

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Blog.Repo.insert!(%Blog.Posts.Post{
+  title: "Some Title",
+  subtitle: "Some Subtitle",
+  content: "Some Content"
+})
